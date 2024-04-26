692fed5804a0f3001cbafb4088d126fca7e4082a
ginni jain <98014085+ginni16@users.noreply.github.com>
2024-04-26 13:25:27 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,7 +2,20 @@ function smallestMissingPositiveInteger(nums) {
 
 
     // Implement the function smallest_missing_positive_integer
-  
+     nums = nums.filter((num, index) => num > 0 && nums.indexOf(num) === index);
+    
+    // Sort the array
+    nums.sort((a, b) => a - b);
+
+    let missing_num = 1;
+    for (const num of nums) {
+        if (num === missing_num) {
+            missing_num++;
+        } else {
+            return missing_num;
+        }
+    }
+    return missing_num;
   }
   
   module.exports = smallestMissingPositiveInteger;
